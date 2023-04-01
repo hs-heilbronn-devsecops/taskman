96eedd48d3f7da5b3f0f1f59db94ba2a1e25efa2
kheireddine Azzez <kheireddine.azzez@etudiant-isi.utm.tn>
2023-04-01 11:34:21 +0000

    Create welcome page

--- taskman/main.py
+++ taskman/main.py
@@ -3,6 +3,7 @@ from typing import Dict, List
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -19,8 +20,17 @@ class Task(TaskRequest):
 tasks: Dict[str, Task] = {}
 
 @app.get('/')
-def home():
-    return {"message":"home"}
+def home(response_class=HTMLResponse):
+    html_content = """ <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>hs-heilbronn-devsecops-tigers</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
   
 
 @app.get('/tasks')
