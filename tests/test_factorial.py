bb65ab7c4400b9d9a5559efc5aaa0551a5b6a571
Yash Bhuva <yashbhuva2412@gmail.com>
2023-04-12 11:03:52 +0000

    Auto test on pull request

---
+++ tests/test_factorial.py
@@ -0,0 +1,11 @@
+def test_factorial():
+    assert factorial(0) == 1
+    assert factorial(1) == 1
+    assert factorial(8) == 40320 
+    assert factorial(12) == 479001600
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
