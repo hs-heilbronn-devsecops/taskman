9d8304af8b81e1ad93101606ab629d6915b33531
shaista jabeen <shaista.jabeen0312@gmail.com>
2023-04-24 22:13:11 +0000

    new commit

--- tests/test_pattern.py
+++ tests/test_pattern.py
@@ -4,9 +4,9 @@ def test_feature_a():
     assert 2 * 2 == 4
 
 def test_feature_b():
-    assert "Hello, world!"[0:5] == "Hello"  
+    assert "Hello, world!"[0:5] == "Hello"    
 
 def test_feature_c():
     assert len([1, 2, 3, 4, 5]) == 5
 
-    
+
